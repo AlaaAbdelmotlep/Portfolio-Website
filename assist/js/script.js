a11734bb67bfd0113af97c2d1e14bc8549464d09
AlaaAbdelmotlep <52351170+AlaaAbdelmotlep@users.noreply.github.com>
2021-09-23 04:21:13 +0200

    services-section

--- assist/js/script.js
+++ assist/js/script.js
@@ -108,6 +108,30 @@ tabs.forEach((tab) => {
 
 // ************************ SERVICES MODEL **********************
 
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
+
+// fun accept index , and add active class to modelView of this index
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+// when enter closse btn , make sure all modelView is closed
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
+
 // ************************ PROTOFOLIO SWIPER **********************
 
 // ************************ TESTIMONIAL **********************
